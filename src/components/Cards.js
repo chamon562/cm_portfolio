fefee7c2970ad009a62b2609c9f7a1d4ac0d25c0
Chamon562 <Chamon562@gmail.com>
2021-01-03 14:31:26 -0800

    [Cards.js] Trying to link image but not showing and had some ideas on what I wanted to put in along with my project cards.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,25 @@
 import React from "react";
+import Carditem from "./Carditem";
+import "./Cards.css";
 
 function Cards() {
     return (
         <div className="cards">
-            <h1>Check out these Destiniations</h1>
+            <h1>My Projects</h1>
             <div className="cards_container">
                 <div className="cards_wrapper">
                     <div className="cards_items">
-
+                        {/* passing in props by setting in the value on the cards component 
+                        ans pass props from the cardsitem*/}
+                        <Carditem 
+                        src="images/img-9.jpg"
+                        text="This project helped me understand how to use express.js
+                        I also remember understanding more on what backend and front actually meant.
+                        I enjoyed learning how to use the node package axios to get grab and display information
+                        from a an api I found googling. This project was made with. note put an icon or logo of all the languages used to make this project. "
+                        label="project"
+                        path="/projects"
+                        />
                     </div>
                 </div>
             </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "../../App.css";
+import Cards from "../Cards";
 import HeroSection from "../HeroSection"
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-        <HeroSection />
+            <HeroSection />
+            <div>
+                {/* save this for later to show how was able to pull out of a depressive state and keep going  */}
+                <h1>My Story</h1>
+                <p>It was a tough time trying to stay positive through Covid times.
+                But I know everything I've been through during my troubled youth,
+                that I was prepared to want, and must achieve my Goals to be a better me.
+                </p>
+            </div>
+            <Cards />
         </>
     )
 }
