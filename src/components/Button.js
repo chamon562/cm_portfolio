288645a6a6ff753e3b77f870cc604c4f2016b55d
Chamon562 <Chamon562@gmail.com>
2021-01-02 20:46:54 -0800

    [Button.js] Add sizes and styles with 2 turneraries and a link with a button that has children in it.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,22 @@ import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
 
-function Button(){
-    return(
-        <div>
+const STYLES = ["btn--primary", "btn--outline"];
 
+const SIZES = ["btn--medium", "btn--large"];
+
+function Button(children, type, onClick, buttonStyle, buttonSize) {
+    const checkButtonStyle = STYLES.infludes(buttonStyle) ? buttonStyle : STYLE[0]
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZE[0];
+    return (
+        <div>
+            <Link to="" className="btn-mobile">
+                <button>
+                    {/* whatever put in button it will render that*/}
+                {children}
+                </button>
+            </Link>
         </div>
     )
 }
