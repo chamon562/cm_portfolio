bf1a0ec653e1d0e09c50cab479f797214d8ee9d6
Chamon562 <Chamon562@gmail.com>
2021-01-02 19:33:52 -0800

    [Navbar.js] Add in basic function component of Navbar with a nav tag, a div, and link tag, also instlled react-router-dom node package.

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function Navbar() {
+    return (
+        <>
+            {/* jsx goes here <> fragments </>*/}
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <link></link>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
