4f44af102ba4199215778bdf45077f1955d41dc0
Chamon562 <Chamon562@gmail.com>
2021-01-02 20:01:59 -0800

    [app.js] Add import of Navbar, BrowserRouter and put in components with a kitty logo for home.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <>
             {/* jsx goes here <> fragments </>*/}
             <nav className="navbar">
                 <div className="navbar-container">
-                    <link></link>
+                    <Link to="/" className="navbar-logo">
+                        Home <i class="fas fa-cat"></i>  
+                    </Link>
                 </div>
             </nav>
         </>
